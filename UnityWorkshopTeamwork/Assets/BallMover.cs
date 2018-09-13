5ef29535cab068e2981327f41f823c5d82f75142
Rickbieze <rickbieze@hotmail.com>
2018-09-13 15:55:59 +0200

    jumper
    zonder double jump

--- UnityWorkshopTeamwork/Assets/BallMover.cs
+++ UnityWorkshopTeamwork/Assets/BallMover.cs
@@ -7,20 +7,38 @@ public class BallMover : MonoBehaviour {
     // Use this for initialization
     public float speed;
 
+    private bool isGrounded;
+
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isGrounded = true;
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(new Vector3(0, 3, 0), ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
     }
 }

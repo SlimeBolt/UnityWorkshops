36e7a1c3bd948ba3702edf40f64cd6ecbbd9a76c
Rickbieze <rickbieze@hotmail.com>
2018-09-13 16:31:17 +0200

    timer added
    nu de rest nog

---
+++ UnityWorkshopTeamwork/Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    // Use this for initialization
+    private Text m_MyText;
+    private bool isFinished;
+    private string finishedTime;
+
+    void Start()
+    {
+        //Text sets your text to say this message
+        m_MyText = GetComponent<Text>();
+        isFinished = false;
+    }
+
+    void Update()
+    {
+        if (!isFinished)
+        {
+            m_MyText.text = Time.time.ToString();
+        }
+        if(isFinished)
+        {
+            finishedTime = Time.time.ToString();
+        }
+    }
+}

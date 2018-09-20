73771f8dace69b2e1f6049919c71269c07173461
Rickbieze <rickbieze@hotmail.com>
2018-09-20 16:11:25 +0200

    begin enemy finite state machine
    werkt nog niet >.>

---
+++ UnityWorkshop2/Assets/EventDispatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventDispatcher : MonoBehaviour {
+
+    public UnityEvent moveToPlayer;
+
+    // public StateTransitionRequestEvent request = new StateTransitionRequestEvent();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

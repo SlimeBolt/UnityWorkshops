f97c868b89b34bb161a02361d67eb55d7de3b2d5
SlimeBolt <dion.ooms@live.nl>
2018-09-20 16:20:25 +0200

    Player Health

--- UnityWorkshop2/Assets/Scripts/PlayerHealth.cs
+++ UnityWorkshop2/Assets/Scripts/PlayerHealth.cs
@@ -4,34 +4,26 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class PlayerHealth : MonoBehaviour
+[CreateAssetMenu(fileName = "Health", menuName = "Player/Health", order = 1)]
+public class PlayerHealth : ScriptableObject
 {
     public int startingHealth = 100;                            // The amount of health the player starts the game with.
     public int currentHealth;                                   // The current health the player has.
     public Slider healthSlider;                                 // Reference to the UI's health bar.
     public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
-    public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
 
-
-    AudioSource playerAudio;                                    // Reference to the AudioSource component.
     bool isDead;                                                // Whether the player is dead.
     bool damaged;                                               // True when the player gets damaged.
 
-
-    void Awake()
+    public void Reset()
     {
-        // Setting up the references.
-        playerAudio = GetComponent<AudioSource>();
-
-
         // Set the initial health of the player.
         currentHealth = startingHealth;
     }
 
-
-    void Update()
+    public void UpdateHealth()
     {
         // If the player has just been damaged...
         if (damaged)
@@ -50,7 +42,7 @@ public class PlayerHealth : MonoBehaviour
         damaged = false;
     }
 
-        public void TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         // Set the damaged flag so the screen will flash.
         damaged = true;
@@ -61,34 +53,11 @@ public class PlayerHealth : MonoBehaviour
         // Set the health bar's value to the current health.
         healthSlider.value = currentHealth;
 
-        // Play the hurt sound effect.
-        playerAudio.Play();
-
         // If the player has lost all it's health and the death flag hasn't been set yet...
         if (currentHealth <= 0 && !isDead)
         {
             SceneManager.LoadScene("Scenes/GameOver");
-            // ... it should die.
-            Death();
-            
+            isDead = true;
         }
-    }
-
-    void OnMouseDown()
-    {
-        TakeDamage(5);
-        print("You are damaged");
-    }
-
-    void Death()
-    {
-        // Set the death flag so this function won't be called again.
-        isDead = true;
-
-        // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
-        playerAudio.clip = deathClip;
-        playerAudio.Play();
-
-        
     }
 }

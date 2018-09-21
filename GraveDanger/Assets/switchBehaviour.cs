e1dd4b3208c72e12628f28085b2ca2823ae604d1
SummerRoses <juliette.terhaar@hotmail.com>
2018-09-21 09:25:56 +0200

    "Final game"

---
+++ GraveDanger/Assets/switchBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchBehaviour : MonoBehaviour {
+
+
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var script = other.gameObject.GetComponent<AgentBehaviour>();
+            if (script.keycard1 && gameObject.CompareTag("switch"))
+            {
+
+                gameObject.SetActive(false);
+            }
+            if (script.keycard2 && gameObject.CompareTag("switch1"))
+            {
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}

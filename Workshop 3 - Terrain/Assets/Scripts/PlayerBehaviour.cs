1e18230a0c9929e1345ab0e337e67e9f5ce2a824
Dion <dion.ooms@live.nl>
2018-10-04 15:48:18 +0200

    Created Inventory System + Switches/Doors

---
+++ Workshop 3 - Terrain/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    private InventoryBehaviour inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = GetComponent<InventoryBehaviour>();
+    }
+	
+	public InventoryBehaviour GetInventory()
+    {
+        return inventory;
+    }
+}

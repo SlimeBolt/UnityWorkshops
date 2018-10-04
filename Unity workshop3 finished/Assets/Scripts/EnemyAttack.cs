2414d295429d8e78f7e402ab2d7d18df5a227b8e
SummerRoses <juliette.terhaar@hotmail.com>
2018-10-04 21:48:16 +0200

    Unity workshop van vandaag, inclusief build .exe

---
+++ Unity workshop3 finished/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    public int attackDamage = 10;               // The amount of health taken away per attack.
+
+
+
+    GameObject player;                          // Reference to the player GameObject.
+    PlayerHealth playerHealth;                  // Reference to the player's health.
+
+    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            if (playerHealth.currentHealth > 0)
+            {
+                // ... damage the player.
+                playerHealth.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+}
+    
---
+++ Unity workshop3 finished/Assets/Scripts/SwitchBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBehaviour : MonoBehaviour {
+    public InventoryItemBehaviour[] keyList;
+
+    private bool isOpen = false;
+
+    public bool isActive()
+    {
+        return !isOpen;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isOpen)
+        {
+            InventoryBehaviour playerInventory = other.gameObject.GetComponent<PlayerBehaviour>().GetInventory();
+
+            // Check if all keys for this switch are in the inventory
+            foreach(InventoryItemBehaviour key in keyList)
+            {
+                isOpen = playerInventory.InInventory(key);
+                if (!isOpen)
+                {
+                    break;
+                }
+            }
+
+            // Set the object inactive if it is open
+            gameObject.SetActive(!isOpen);
+        }
+    }
+}
